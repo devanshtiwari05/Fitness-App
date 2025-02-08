c0315d440c7e392d1c6a5e02da3877dd45d1dc5c
devanshtiwari05 <devansht150@gmail.com>
2025-02-08 18:44:41 +0530

    Version 1.2 of Fitness App

---
+++ app/src/main/java/com/example/fitnessapp/Response/AIResponse.kt
@@ -0,0 +1,7 @@
+package com.example.fitnessapp.Response
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun GetResponse(navController: NavController){}
