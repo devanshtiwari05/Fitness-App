c6d5cd329ad5508c2f81d23860d9d49adf4c35e6
devanshtiwari05 <devansht150@gmail.com>
2025-02-23 17:13:40 +0530

    Version 1.6 of Fitness App

---
+++ app/src/main/java/com/example/fitnessapp/di/AuthModule.kt
@@ -0,0 +1,32 @@
+package com.example.fitnessapp.di
+
+import android.app.Application
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AuthModule {
+
+    @Provides
+    @Singleton
+    fun provideFirebaseAuth(): FirebaseAuth = FirebaseAuth.getInstance()
+
+    @Provides
+    @Singleton
+    fun provideFirebaseFirestore(): FirebaseFirestore = FirebaseFirestore.getInstance()
+
+    @Provides
+    @Singleton
+    fun provideSignInClient(application: Application): SignInClient {
+        return Identity.getSignInClient(application)
+    }
+
+}
